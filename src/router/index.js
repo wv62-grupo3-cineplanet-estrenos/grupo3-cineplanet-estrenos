ddb1fe6ad79a951fb7c628fbf62be14ff8439423
Juan Diego Cueto Dominguez <u202012207@upc.edu.pe>
2024-06-16 17:58:08 -0500

    feat: book a movie page added

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import {createRouter, createWebHistory} from "vue-router";
 import LoginComponent from "../../src/public/pages/login.component.vue";
 import MoviesComponent from "../../src/public/pages/Movies.component.vue";
 import RegisterComponent from "../../src/public/pages/Register.component.vue";
+import BookAMovieComponent from  "../../src/cineplanet/pages/BookAMovie.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -11,7 +12,11 @@ const router = createRouter({
 
         {path: '/salir', redirect: '/login'},
 
-        {path: '/movies', component: MoviesComponent}
+        {path: '/movies', component: MoviesComponent, name: 'movies'},
+
+        {path:'/book-a-movie', component: BookAMovieComponent},
+
+        {path: '/', redirect: '/login'}
 
     ]
 });
