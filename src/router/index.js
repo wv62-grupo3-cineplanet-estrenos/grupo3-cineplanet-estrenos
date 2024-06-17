94d5dbc9880a6af935cb57eb69452b8d761fcb18
Racso1626 <oscararmas2016@gmail.com>
2024-06-16 20:48:49 -0500

    feat: login, register and movies components implemented.

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,19 @@
-import {createRouter, createWebHistory} from "vue-router";
-import LoginComponent from "../../src/public/pages/login.component.vue";
-import MoviesComponent from "../../src/public/pages/Movies.component.vue";
-import RegisterComponent from "../../src/public/pages/Register.component.vue";
-import BookAMovieComponent from  "../../src/cineplanet/pages/BookAMovie.component.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import LoginComponent from '../../src/public/pages/login.component.vue';
+import MoviesComponent from '../../src/public/pages/movies.component.vue';
+import RegisterComponent from '../../src/public/pages/register.component.vue';
+import BookingComponent from '../../src/public/pages/booking.component.vue'; // Asegúrate de que esta ruta sea correcta
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        {path: '/login', component: LoginComponent},
-        {path: '/register', component: RegisterComponent },
-
-        {path: '/salir', redirect: '/login'},
-
-        {path: '/movies', component: MoviesComponent, name: 'movies'},
-
-        {path:'/book-a-movie', component: BookAMovieComponent},
-
-        {path: '/', redirect: '/login'}
-
-    ]
+        { path: '/login', component: LoginComponent },
+        { path: '/register', component: RegisterComponent },
+        { path: '/salir', redirect: '/login' },
+        { path: '/movies', component: MoviesComponent },
+        { path: '/booking', component: BookingComponent }, // Añadido para la vista de booking
+        { path: '/', redirect: '/login' }, // Redirigir a login por defecto
+    ],
 });
 
 export default router;
